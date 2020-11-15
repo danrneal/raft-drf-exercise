46735231ae0d922cedc637f34f8c25084b018f7f
Daniel R. Neal <dan.r.neal@gmail.com>
2020-11-14 16:41:37 -0800

    fix: Specify correct read upload url

--- upload_server/urls.py
+++ upload_server/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("api/upload", views.CreateUpload.as_view(), name="create_upload"),
-    path("api/<int:upload_id>", views.read_upload, name="read_upload"),
+    path("api/upload/<int:upload_id>", views.read_upload, name="read_upload"),
 ]
